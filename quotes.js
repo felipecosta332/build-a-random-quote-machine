2baf727b15575a6fa616926fe71a620262418bf4
Felipe Costa <felipecosta53@info.ufrn.br>
2021-01-02 11:55:31 -0300

    bootstrap styling

---
+++ quotes.js
@@ -0,0 +1,66 @@
+const QUOTEBANK = [
+  {
+      author: 'Japanese Proverb',
+      quote: 'Fall seven times and stand up eight.'
+  },
+  {
+      author: 'Marilyn Monroe',
+      quote: 'I\'m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can\'t handle me at my worst, then you sure as hell don\'t deserve me at my best.'
+  },
+  {
+      author: 'Oscar Wilde',
+      quote: 'Be yourself; everyone else is already taken.'
+  },
+  {
+      author: 'Albert Einstein',
+      quote: 'Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.'
+  },
+  {
+      author: 'Frank Zappa',
+      quote: 'So many books, so little time.'
+  },
+  {
+      author: 'Marcus Tullius Cicero',
+      quote: 'A room without books is like a body without a soul.'
+  },
+  {
+      author: 'Bernard M. Baruch',
+      quote: 'Be who you are and say what you feel, because those who mind don\'t matter, and those who matter don\'t mind.'
+  },
+  {
+      author: 'William W. Purkey',
+      quote: 'You\'ve gotta dance like there\'s nobody watching,\nLove like you\'ll never be hurt,\nSing like there\'s nobody listening,\nAnd live like it\'s heaven on earth.'
+  },
+  {
+      author: 'Dr. Seuss',
+      quote: 'You know you\'re in love when you can\'t fall asleep because reality is finally better than your dreams.'
+  },
+  {
+      author: 'Mae West',
+      quote: 'You only live once, but if you do it right, once is enough.'
+  },
+  {
+      author: 'Mahatma Gandhi',
+      quote: 'Be the change that you wish to see in the world.'
+  },
+  {
+      author: 'Robert Frost',
+      quote: 'In three words I can sum up everything I\'ve learned about life: it goes on.'
+  },
+  {
+      author: 'J.K. Rowling, Harry Potter and the Goblet of Fire',
+      quote: 'If you want to know what a man\'s like, take a good look at how he treats his inferiors, not his equals.'
+  },
+  {
+      author: 'Albert Camus',
+      quote: 'Don’t walk in front of me… I may not follow\nDon’t walk behind me… I may not lead\nWalk beside me… just be my friend'
+  },
+  {
+      author: 'Eleanor Roosevelt, This is My Story',
+      quote: 'No one can make you feel inferior without your consent.'
+  },
+  {
+      author: 'Mark Twain',
+      quote: 'If you tell the truth, you don\'t have to remember anything.'
+  }
+];
